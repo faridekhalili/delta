6f0076b6dd890e79a95de0afce59b6633127f184
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2025-01-25 14:05:29 -0500

    Kills delta 9

--- test/delta/helpers.ts
+++ test/delta/helpers.ts
@@ -1,4 +1,5 @@
 import Delta from '../../src/Delta';
+import AttributeMap from '../../src/AttributeMap';
 
 describe('helpers', () => {
   describe('concat()', () => {
@@ -39,6 +40,19 @@ describe('helpers', () => {
       const delta = new Delta().insert('Test');
       const expected = new Delta().insert('Test');
       expect(delta.chop()).toEqual(expected);
+    });
+
+    it('delta_9', () => {
+      /**
+        ConditionalExpression
+        src/Delta.ts:80:7
+        -         typeof attributes === 'object' &&
+        +         true &&
+       */
+      const a = new Delta();
+      const b = new Delta([{ insert: 'foo' }]);
+      const attributes: AttributeMap = <AttributeMap><unknown>'bar';
+      expect(a.insert('foo', attributes)).toEqual(b)
     });
 
     it('formatted retain', () => {
