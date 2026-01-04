cd154fbc310ea9da6d60908ecd374604b441f5e1
Faridekhalili <khalili.f@northeastern.edu>
2026-01-03 21:32:49 -0500

    Add missing test.

--- test/augmented.test.ts
+++ test/augmented.test.ts
@@ -31,6 +31,31 @@ describe('Added tests', () => {
         expect(source.compose(other)).toEqual(expected);
     });
 
+    it('Sample 3', () => {
+        /**
+         * Sample 3
+         * src/Delta.ts:248:33
+         * -           thisIter.peekType() === 'insert' &&
+         * +           thisIter.peekType() === "" &&
+         */
+        const left = new Delta([
+            { insert: 'A' },
+            { insert: 'B' },
+            { insert: 'A' },
+        ]);
+        const right = new Delta([
+            { retain: 2 },
+            { retain: 2, attributes: { bold: true } },
+        ]);
+        const expected = new Delta([
+            { insert: 'A' },
+            { insert: 'B' },
+            { insert: 'A', attributes: { bold: true } },
+            { retain: 1, attributes: { bold: true } },
+        ]);
+        expect(left.compose(right)).toEqual(expected);
+    });
+
     it('Sample 4', () => {
         /**
          * Sample 4
